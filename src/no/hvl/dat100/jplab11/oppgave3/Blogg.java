b79b70ff5a872ff877be1825889c25625056b637
H670662 <142589240+H670662@users.noreply.github.com>
2023-11-04 00:44:28 +0100

    Blogg v0.1
    ikke klar

--- src/no/hvl/dat100/jplab11/oppgave3/Blogg.java
+++ src/no/hvl/dat100/jplab11/oppgave3/Blogg.java
@@ -5,46 +5,79 @@ import no.hvl.dat100.jplab11.oppgave1.*;
 
 public class Blogg {
 
-	// TODO: objektvariable 
+	protected Innlegg[] innleggsTabell;
+	protected int nesteLedig;
 
 	public Blogg() {
-		throw new UnsupportedOperationException(TODO.constructor("Blogg"));
+		this.innleggsTabell = new Innlegg[20];
+		this.nesteLedig = 0;
 	}
 
 	public Blogg(int lengde) {
-		throw new UnsupportedOperationException(TODO.constructor("Blogg"));
+		this.innleggsTabell = new Innlegg[lengde];
+		this.nesteLedig = 0;
 	}
 
 	public int getAntall() {
-		throw new UnsupportedOperationException(TODO.method());
+		return this.nesteLedig;
 	}
 	
 	public Innlegg[] getSamling() {
-		throw new UnsupportedOperationException(TODO.method());
+		return innleggsTabell;
 
 	}
 	
 	public int finnInnlegg(Innlegg innlegg) {
-
-		throw new UnsupportedOperationException(TODO.method());
+		if (this.nesteLedig == 0) {
+			return -1;
+		}
+		for (int i = 0; i< innleggsTabell.length; i ++) {
+			if ( this.innleggsTabell[i].getId() == innlegg.getId()) {
+				return i;
+			} 
+		}
+		return -1;
 	}
 
 	public boolean finnes(Innlegg innlegg) {
-		throw new UnsupportedOperationException(TODO.method());
+		if (this.nesteLedig == 0) {
+			return false;
+		}
+		for (int i = 0; i< innleggsTabell.length; i ++) {
+			if ( this.innleggsTabell[i].getId() == innlegg.getId()) {
+				return true;
+			} 
+		}
+		return false;
 	}
 
 	public boolean ledigPlass() {
-		throw new UnsupportedOperationException(TODO.method());
+		if (this.nesteLedig > this.innleggsTabell.length) {
+			return true;
+		} else return false;
 
 	}
 	
 	public boolean leggTil(Innlegg innlegg) {
-
-		throw new UnsupportedOperationException(TODO.method());
+		if (this.nesteLedig+1 == this.innleggsTabell.length) {
+			return false;
+		}
+		
+		if (finnes(innlegg) == true) {
+			return false;
+		}
+		
+		this.innleggsTabell[nesteLedig] = innlegg;
+		nesteLedig++;
+		return true;
 	}
 	
 	public String toString() {
-		throw new UnsupportedOperationException(TODO.method());
+		String s = "";
+		for (Innlegg i : innleggsTabell) {
+			s += i.toString() + "\n";
+		}
+		return s;
 	}
 
 	// valgfrie oppgaver nedenfor
