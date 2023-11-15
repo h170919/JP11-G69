7dcc86259645bca5b492ec120c65c894d13ca287
h170919 <52938064+h170919@users.noreply.github.com>
2023-11-15 18:24:54 +0100

    fullførte oppgave 4

--- src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
+++ src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
@@ -2,14 +2,19 @@ package no.hvl.dat100.jplab11.oppgave4;
 
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-
-import no.hvl.dat100.jplab11.common.TODO;
 import no.hvl.dat100.jplab11.oppgave3.*;
 
 public class SkrivBlogg {
 
-	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
-
-		throw new UnsupportedOperationException(TODO.method());
-	}
+    public static boolean skriv(Blogg samling, String mappe, String filnavn) {
+        try {
+            PrintWriter writer = new PrintWriter(mappe + filnavn);
+            writer.print(samling.toString());
+            writer.close();
+            return true;
+        } catch (FileNotFoundException e) {
+            System.err.println("Feil ved skriving til fil: " + e.getMessage());
+            return false;
+        }
+    }
 }
